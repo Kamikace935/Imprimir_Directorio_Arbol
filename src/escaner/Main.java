5f6d3237fb6ed989f0a6ba082c3f45758fbdf917
Pablo Marian <pablomarianv@gmail.com>
2023-04-03 19:01:17 +0200

    Fix Fichero null

--- src/escaner/Main.java
+++ src/escaner/Main.java
@@ -2,7 +2,6 @@ package escaner;
 
 import java.io.File;
 
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
@@ -11,49 +10,49 @@ public class Main {
         File fichero;
         System.out.print("Introduce la ruta que quieres revisar: ");
         fichero = new File(teclado.nextLine());
-
         System.out.print(imprimeDirectorioArbol(fichero));
-
         teclado.close();
     }
-
 
     public static String imprimeDirectorioArbol(File fichero){
         if (!fichero.isDirectory()){
             throw new IllegalArgumentException("El fichero no es un directorio");
         }
-        int posicion = 0;
+        int sangrado = 0;
         StringBuilder sb = new StringBuilder();
-        imprimeDirectorioArbol(fichero, posicion, sb);
+        imprimeDirectorioArbol(fichero, sangrado, sb);
         return sb.toString();
     }
 
-    private static void imprimeDirectorioArbol(File fichero, int posicion, StringBuilder sb) {
+    private static void imprimeDirectorioArbol(File fichero, int sangrado, StringBuilder sb) {
          if(!fichero.isDirectory()){
              throw new IllegalArgumentException("El fichero no es un directorio");
          }
-         sb.append(posicionString(posicion));
+         sb.append(sangradoString(sangrado));
          sb.append("+--");
          sb.append(fichero.getName());
          sb.append("/");
          sb.append("\n");
-         for(File archivo : fichero.listFiles()) {
-             if(archivo.isDirectory()) {
-                 imprimeDirectorioArbol(archivo, posicion + 1, sb);
-             } else {
-                 imprimirArchivo(archivo, posicion + 1, sb);
+         File[] ficheros = fichero.listFiles();
+         if (ficheros != null) {
+             for (File archivo : ficheros) {
+                 if (archivo.isDirectory()) {
+                     imprimeDirectorioArbol(archivo, sangrado + 1, sb);
+                 } else {
+                     imprimirArchivo(archivo, sangrado + 1, sb);
+                 }
              }
          }
     }
 
-    private static void imprimirArchivo(File archivo, int posicion, StringBuilder sb) {
-        sb.append(posicionString(posicion));
+    private static void imprimirArchivo(File archivo, int sangrado, StringBuilder sb) {
+        sb.append(sangradoString(sangrado));
         sb.append("+--");
         sb.append(archivo.getName());
         sb.append("\n");
     }
 
-    private static String posicionString(int posicion) {
-        return "|  ".repeat(Math.max(0, posicion));
+    private static String sangradoString(int sangrado) {
+        return "|  ".repeat(Math.max(0, sangrado));
     }
 }
